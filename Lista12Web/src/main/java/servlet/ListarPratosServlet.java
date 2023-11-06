264fbb0471060aec6bfd5c3099beb017a78a2520
Guilherme Carvalho <89918329+Fiudy@users.noreply.github.com>
2023-11-06 11:44:22 -0300

    Add files via upload

---
+++ Lista12Web/src/main/java/servlet/ListarPratosServlet.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ListarPratosServlet")
+public class ListarPratosServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+       
+    }
+}
